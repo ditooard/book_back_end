0d1bea551978f9dc4a51284969af2aa461206a0f
hendriwhyu <hendriwp123@gmail.com>
2024-01-25 20:50:22 +0700

    fix: payment gateway callback & redirect url

--- php/payment.php
+++ php/payment.php
@@ -1,7 +1,7 @@
 <?php
-require "billplz.phar";
+// require "../vendor/jomweb/billplz";
 
-use Billplz\Client;
+// use Billplz\Client;
 //error_reporting(0);
 
 $email = $_GET['email']; //email
@@ -27,40 +27,18 @@ $data = [
     'redirect_url' => "https://bookshelf.wedmanagement.com/api/payment_update.php?userid=$userid&email=$email&phone=$phone&amount=$amount&name=$name"
 ];
 
+$process = curl_init($host);
 
-$billplz = Client::make($api_key);
-$billplz->useSandbox();
-$billplz->useVersion('v3');
+curl_setopt($process, CURLOPT_HEADER, 0);
+curl_setopt($process, CURLOPT_USERPWD, $api_key . ":");
+curl_setopt($process, CURLOPT_TIMEOUT, 30);
+curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($process, CURLOPT_POSTFIELDS, http_build_query($data));
 
-$collection = $billplz->collection();
-$collection->get($collection_id);
-
-// Create Bill
-$bill = $billplz->bill();
-$response = $bill->create($data['collection_id'], $data['email'], $data['phone'], $data['name'], $data['amount'], $data['metadata'], $data['callback_url']);
-
-json_encode($response);
-
-
-
-
-
-// $process = curl_init($host);
-
-
-
-
-
-
-// curl_setopt($process, CURLOPT_HEADER, 0);
-// curl_setopt($process, CURLOPT_USERPWD, $api_key . ":");
-// curl_setopt($process, CURLOPT_TIMEOUT, 30);
-// curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-// curl_setopt($process, CURLOPT_SSL_VERIFYHOST, 0);
-// curl_setopt($process, CURLOPT_SSL_VERIFYPEER, 0);
-// curl_setopt($process, CURLOPT_POSTFIELDS, http_build_query($data));
-
-// $return = curl_exec($process);
-// curl_close($process);
-//print_r($bill);
-// header("Location: {$bill['url']}");
+$return = curl_exec($process);
+curl_close($process);
+$bill = json_decode($return, true);
+print_r($bill);
+header("Location: {$bill['url']}");
