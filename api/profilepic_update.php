a0252be360a9e0c0caba188fec98b1b8263dd304
hendriwhyu <hendriwp123@gmail.com>
2024-02-05 16:05:16 +0700

    fix: profile update

--- api/profilepic_update.php
+++ api/profilepic_update.php
@@ -10,13 +10,10 @@ include_once("dbconnect.php"); // include database connection file
 $data = json_decode(file_get_contents("php://input"), true); // collect input parameters and convert into readable format
 
 $userid = $_GET['userid'];
-$fileName  =  $_FILES['avatar']['name'];
-$tempPath  =  $_FILES['avatar']['tmp_name'];
-$fileSize  =  $_FILES['avatar']['size'];
-$fileExt = strtolower(pathinfo($fileName, PATHINFO_EXTENSION)); // get image extension
+$fileName = $_POST['avatar'];
+$decoded_string = base64_decode($fileName);
 
 $hashFileName = sha1($fileName);
-$fileStoreDatabase = $hashFileName . "." . $fileExt;
 
 $sqlGetUser = "SELECT * FROM `tbl_users` WHERE `user_id` = '$userid'";
 $result = $conn->query($sqlGetUser);
@@ -39,30 +36,12 @@ if ($result->num_rows > 0) {
         $errorMSG = json_encode(array("message" => "please select image", "status" => "error"));
         echo $errorMSG;
     } else {
-        $upload_path = '../assets/avatar/'; // set upload folder path 
 
-
+        $path = '../assets/avatar/' . $hashFileName . '.png';
+        file_put_contents($path, $decoded_string);
         // valid image extensions
-        $valid_extensions = array('jpeg', 'jpg', 'png', 'gif');
 
-        // allow valid image file formats
-        if (in_array($fileExt, $valid_extensions)) {
-            // check file size '5MB'
-            if ($fileSize < 5000000) {
-                move_uploaded_file($tempPath, $upload_path . $fileStoreDatabase); // move file from system temporary path to our upload folder path 
-            } else {
-                $errorMSG = json_encode(array("message" => "Sorry, your file is too large, please upload 5 MB size", "status" => "error"));
-                echo $errorMSG;
-            }
-        } else {
-            $errorMSG = json_encode(array("message" => "Sorry, only JPG, JPEG, PNG & GIF files are allowed", "status" => "error"));
-            echo $errorMSG;
-        }
-    }
-
-    // if no error caused, continue ....
-    if (!isset($errorMSG)) {
-        $query = mysqli_query($conn, "UPDATE tbl_users SET user_photo ='$fileStoreDatabase' WHERE user_id='$userid'");
+        $query = mysqli_query($conn, "UPDATE tbl_users SET user_photo ='$path' WHERE user_id='$userid'");
 
         echo json_encode(array("message" => "Image Uploaded Successfully", "status" => "success"));
     }
